173b29e2d84efec5ea8394efd1d972c5e257f4b1
mrazniwaka <niwakasizin@gmail.com>
2020-08-21 08:38:46 +0000

    follow,unfollow

--- app/User.php
+++ app/User.php
@@ -40,12 +40,72 @@ class User extends Authenticatable
     //このユーザが所有する投稿（Micropostモデルとの関係を定義）
     public function microposts()
     {
-        return $this->hasmany(Micropost::class);
+        return $this->hasMany(Micropost::class);
     }
+    //このユーザがフォロー中のユーザ（Userモデルとの関係を定義）
+    public function followings()
+    {
+        return $this->belongsToMany(User::class,'user_follow','user_id','follow_id')->withTimestamps();
+    }
+    
+    //このユーザをフォロー中のユーザ（Userモデルとの関係を定義）
+    public function followers()
+    {
+        return $this->belongsToMany(User::class,'user_follow','follow_id','user_id')->withTimestamps();
+    }
+    
+    //userIdで指定されたユーザをフォローする
+    
+    public function follow($userId)
+    {
+        //すでにフォローしているかの確認
+        $exist = $this->is_following($userId);
+        // 相手が自分かの確認
+        $its_me = $this->id == $userId;
+        
+        if($exist || $its_me){
+            //すでにフオロー指定ればフォローしない
+            return false;
+        }else{
+            //未フォローであればフォローする
+        $this->followings()->attach($userId);
+        return true;    
+        }
+        
+    }
+    
+    //$userIdで指定されたユーザをアンフォローする
+    
+    public function unfollow($userId)
+    {
+        //すでにフォローしているかの確認
+        $exist = $this->is_following($userId);
+        //相手が自分自身かの確認
+        $its_me = $this->id == $userId;
+        
+        if($exist && !$its_me){
+            //すでにフォローしていればフォローを外す
+            $this->followings()->detach($userId);
+            return true;
+        }else{
+            //未フォローであれば何もしない
+            return false;
+        }
+    
+    }
+    
+    //指定された$userIdのユーザをこのユーザがフォロー中かどうか調べる。フォロー中ならtrueを返す。
+    public function is_following($userId)
+    {
+        //フォロー中のユーザの中に$userIdのものが存在するか
+        return $this->followings()->where('follow_id',$userId)->exists();
+    }
+    
+    
     
     //このユーザに関係するモデルの件数をロードする
     public function loadRelationshipCounts()
     {
-        $this->loadCount('microposts');
+        $this->loadCount('microposts','followings','followers');
     }
 }
